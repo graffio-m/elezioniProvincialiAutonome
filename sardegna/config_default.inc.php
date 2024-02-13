6acb03888c8d3c0004cd0b1447d8e093a74c7666
graffio <graffio@lynxlab.com>
2024-02-13 10:30:37 +0100

    creato config_default

---
+++ sardegna/config_default.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Impostazione nomi file e URL
+ */
+
+/**
+ * 
+
+ Path dati per dataWrapper
+ https://www.rainews.it/dl/rainews/elezioni2024/sardegna/dati_elezioni_feb_2024.csv
+
+ Path metadati per dataWrapper
+ https://www.rainews.it/dl/rainews/elezioni2024/sardegna/metadati_sardegna.json
+
+ */
+
+
+// Server SFTP settings
+$sftp_server = 'server.sft.it';
+$sftp_username = 'user';
+$sftp_password = 'password';
+$remote_directory = '/var/www/html/sardegna/';
+
+
+
+$csvFilePath = 'dati_elezioni_feb_2024.csv'; 
+
+ // Specifica il percorso del file JSON
+$jsonFilePath = 'metadati_sardegna.json';
+
+// URL of the JSON data
+$jsonUrl = 'https://amministrazioneaperta.regione.sardegna.it/feedprovasier2019.php';
